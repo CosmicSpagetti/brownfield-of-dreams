a79f692c648a7fb65cf0bc96a8ae22cda261c184
earl-stephens <stephensearl@hotmail.com>
2019-07-12 16:42:22 -0600

    backfilled user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,8 +24,8 @@ RSpec.describe User, type: :model do
       expect(admin.admin?).to be_truthy
     end
   end
-  describe 'Instance methods' do 
-    it '.shake_hand' do 
+  describe 'Instance methods' do
+    it '.shake_hand' do
       user_billy = User.create!(email: 'billy@example.com',
                              first_name: 'Billy',
                              last_name: 'Urrutia',
@@ -38,10 +38,62 @@ RSpec.describe User, type: :model do
                             password: 'password',
                             username: 'earl-stephens',
                             github_token: ENV['EARL_GITHUB_TOKEN'])
-      
+
       user_billy.shake_hand(user_earl)
       user_billy.reload
       expect(user_billy.friend_users).to include(user_earl)
+    end
+
+    it '.grab_videos' do
+      user = create(:user)
+      tutorial1 = create(:tutorial, title: 'How to Tie Your Shoes')
+      tutorial2 = create(:tutorial, title: 'How to Brush Your Teeth')
+      video1 = create(:video, title: 'The Bunny Ears Technique', tutorial: tutorial1, position: 1)
+      video2 = create(:video, title: 'Right Over Left', tutorial: tutorial1, position: 2)
+      video3 = create(:video, title: 'Use Velcro', tutorial: tutorial1, position: 3)
+      video4 = create(:video, title: 'Electric Toothbrush', tutorial: tutorial2, position: 1)
+      uv1 = UserVideo.create!(user_id: user.id, video_id: video1.id)
+      uv2 = UserVideo.create!(user_id: user.id, video_id: video2.id)
+      uv3 = UserVideo.create!(user_id: user.id, video_id: video3.id)
+      uv4 = UserVideo.create!(user_id: user.id, video_id: video4.id)
+
+      video_data = user.grab_videos
+
+      expect(video_data[0]).to be_an_instance_of Video
+      expect(video_data[2].position).to eq(2)
+    end
+
+    it '.grab_tutorial_names' do
+      user = create(:user)
+      tutorial1 = create(:tutorial, title: 'How to Tie Your Shoes')
+      tutorial2 = create(:tutorial, title: 'How to Brush Your Teeth')
+      video1 = create(:video, title: 'The Bunny Ears Technique', tutorial: tutorial1, position: 1)
+      video2 = create(:video, title: 'Right Over Left', tutorial: tutorial1, position: 2)
+      video3 = create(:video, title: 'Use Velcro', tutorial: tutorial1, position: 3)
+      video4 = create(:video, title: 'Electric Toothbrush', tutorial: tutorial2, position: 1)
+      uv1 = UserVideo.create!(user_id: user.id, video_id: video1.id)
+      uv2 = UserVideo.create!(user_id: user.id, video_id: video2.id)
+      uv3 = UserVideo.create!(user_id: user.id, video_id: video3.id)
+      uv4 = UserVideo.create!(user_id: user.id, video_id: video4.id)
+
+      tutorial_data = user.grab_tutorial_names
+
+      expect(tutorial_data[0]).to eq('How to Tie Your Shoes')
+      expect(tutorial_data[1]).to eq('How to Brush Your Teeth')
+    end
+
+    it '.update_activation_status' do
+      user_earl = User.create!(email: 'earl@example.com',
+                            first_name: 'Earl',
+                            last_name: 'Stephens',
+                            password: 'password',
+                            username: 'earl-stephens',
+                            github_token: ENV['EARL_GITHUB_TOKEN'],
+                            email_active: false)
+
+      user_earl.update_activation_status
+
+      expect(user_earl.email_active).to eq(true)
     end
   end
 end
