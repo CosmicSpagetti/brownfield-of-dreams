efe1c6d0e0b45e53742910cf1440a3e1ab25423d
billy urrutia <billyurrutia@gmail.com>
2019-07-12 17:05:33 -0600

    More fixing and getting simplecov up
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>

---
+++ spec/models/you_tube/video_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe YouTube::Video do
+  describe 'attributes' do 
+    it 'should display attributes' do 
+      
+    end
+  end
+end
+# :items=>
+#   [{:kind=>"youtube#video",
+#     :etag=>"\"XI7nbFXulYBIpL0ayR_gDh3eu1k/SJDrIfGc7itS0jdLd1wv3keQCpk\"",
+#     :id=>"J7ikFUlkP_k",
+#     :snippet=>
+#      {:publishedAt=>"2018-07-23T20:59:59.000Z",
+#       :channelId=>"UC2zYYOtckevoWTGDu5SdCkg",
+#       :title=>"Rails Integration Tests for Pages with JavaScript",
+#       :description=>
+#        "This video shows how to write feature/integration tests within RSpec configured to run expectations for pages that run JavaScript.",
+#       :thumbnails=>
+#        {:default=>{:url=>"https://i.ytimg.com/vi/J7ikFUlkP_k/default.jpg", :width=>120, :height=>90},
+#         :medium=>{:url=>"https://i.ytimg.com/vi/J7ikFUlkP_k/mqdefault.jpg", :width=>320, :height=>180},
+#         :high=>{:url=>"https://i.ytimg.com/vi/J7ikFUlkP_k/hqdefault.jpg", :width=>480, :height=>360}
--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -18,9 +18,13 @@ describe GithubService do
                       github_token: ENV['BILLY_GITHUB_TOKEN'])
         github = GithubService.new(user)
 
-        repos = github.repos
+        repos = github.repos.first
 
-        expect(repos).to be_a Array
+        expect(repos).to be_a Hash
+        expect(repos).to have_key(:name)
+        expect(repos).to have_key(:html_url)
+        expect(repos[:name]).to eq("battleship")
+        expect(repos[:html_url]).to eq("https://github.com/bplantico/battleship")
       end
     end
   end
@@ -33,9 +37,13 @@ describe GithubService do
                       github_token: ENV['BILLY_GITHUB_TOKEN'])
         github = GithubService.new(user)
 
-        followers = github.followers
+        followers = github.followers.first
 
-        expect(followers).to be_a Array
+        expect(followers).to be_a Hash
+        expect(followers).to have_key(:login)
+        expect(followers).to have_key(:html_url)
+        expect(followers[:login]).to eq("kylecornelissen")
+        expect(followers[:html_url]).to eq("https://github.com/kylecornelissen")
       end
     end
   end
@@ -48,9 +56,13 @@ describe GithubService do
                       github_token: ENV['BILLY_GITHUB_TOKEN'])
         github = GithubService.new(user)
 
-        following = github.following
+        following = github.following.first
 
-        expect(following).to be_a Array
+        expect(following).to be_a Hash
+        expect(following).to have_key(:login)
+        expect(following).to have_key(:html_url)
+        expect(following[:login]).to eq("earl-stephens")
+        expect(following[:html_url]).to eq("https://github.com/earl-stephens")
       end
     end
   end
