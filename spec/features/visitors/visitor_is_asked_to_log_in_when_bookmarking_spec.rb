f10f712113dde1f98fce6a8618f8ce537173656e
billy urrutia <billyurrutia@gmail.com>
2019-07-13 13:59:06 -0600

    User_story_2 Tested and completed
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>

--- spec/features/visitors/visitor_is_asked_to_log_in_when_bookmarking_spec.rb
+++ spec/features/visitors/visitor_is_asked_to_log_in_when_bookmarking_spec.rb
@@ -3,14 +3,12 @@
 require 'rails_helper'
 
 describe 'visitor visits video show page', :js do
-  it 'clicks on the bookmark page and is sent to the log in page' do
+  it 'clicks on the bookmark page and is given a flash message to log it' do
     tutorial = create(:tutorial)
     video = create(:video, tutorial_id: tutorial.id)
 
     visit tutorial_path(tutorial)
 
-    click_on 'Bookmark'
-
-    expect(current_path).to eq(login_path)
+    expect(page).to have_content("User must log in to Bookmark Videos.")
   end
 end
