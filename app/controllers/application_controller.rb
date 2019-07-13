20d5d80debee97c22d10067d38fe71f29ec77b97
billy urrutia <billyurrutia@gmail.com>
2019-07-13 14:04:30 -0600

    SORRY RUBOCOP FOR THE SINGLE SPACE
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 # application controller class
 class ApplicationController < ActionController::Base
   helper_method :current_user
-  
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
